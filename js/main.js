178f3cbb9796e04005126fffaf1f15ab4c7079ba
Tanya <82pripri82@gmail.com>
2021-03-26 21:01:22 +0200

    Menu script

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
-$(function () {
-    // Пустые ссылки
-    $("a[href='#']").click(function (e) {
-        e.preventDefault();
-    });
+document.addEventListener('click', function (e) {
+    if (e.target.href === '#' || e.target.closest(`[href="#"]`)) e.preventDefault();
+});
 
-    // Мобильное меню
-    $("#menu").click(function () {
-        $("#header nav").toggleClass("visible");
-    });
+const
+    menuOpen = document.querySelector('.menu-open'),
+    menuClose = document.querySelector('.menu-close'),
+    menuList = document.querySelector('#header nav ul')
+;
+
+menuOpen.addEventListener('click', function (e) {
+    menuOpen.hidden = true;
+    menuClose.hidden = false;
+    menuList.hidden = false;
+});
+
+menuClose.addEventListener('click', function (e) {
+    menuOpen.hidden = false;
+    menuClose.hidden = true;
+    menuList.hidden = true;
 });
