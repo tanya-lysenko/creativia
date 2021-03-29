7ae4fb6476dced9e87044e6af66033e0e229836f
Tanya <82pripri82@gmail.com>
2021-03-29 20:38:10 +0300

    Details script

--- js/main.js
+++ js/main.js
@@ -2,11 +2,9 @@ document.addEventListener('click', function (e) {
     if (e.target.href === '#' || e.target.closest(`[href="#"]`)) e.preventDefault();
 });
 
-const
-    menuOpen = document.querySelector('.menu-open'),
-    menuClose = document.querySelector('.menu-close'),
-    menuList = document.querySelector('#header nav ul')
-;
+const menuOpen = document.querySelector('.menu-open');
+const menuClose = document.querySelector('.menu-close');
+const menuList = document.querySelector('#header nav ul');
 
 menuOpen.addEventListener('click', function (e) {
     menuOpen.hidden = true;
@@ -18,4 +16,21 @@ menuClose.addEventListener('click', function (e) {
     menuOpen.hidden = false;
     menuClose.hidden = true;
     menuList.hidden = true;
+});
+
+document.addEventListener('click', function (e) {
+    const currentSpoiler = e.target.closest('details.card');
+    const spoilers = document.querySelectorAll('details.card');
+
+    if (currentSpoiler) {
+        for (let spoiler of spoilers) {
+            if (spoiler !== currentSpoiler) {
+                spoiler.removeAttribute('open');
+            }
+        }
+    } else {
+        for (let spoiler of spoilers) {
+            spoiler.removeAttribute('open');
+        }
+    }
 });
